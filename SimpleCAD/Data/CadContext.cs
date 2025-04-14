6052ad21c86b6ba059c878bdade089492888c8c9
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-04-14 19:45:42 +0300

    Create CadContext for DB.

---
+++ SimpleCAD/Data/CadContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCAD.Models;
+
+namespace SimpleCAD.Data
+{
+    public class CadContext : DbContext
+    {
+        public DbSet<Drawing> Drawings { get; set; }
+        public DbSet<Shape> Shapes { get; set; }
+
+        public CadContext(DbContextOptions<CadContext> options) : base(options) { }
+    }
+}
