03f788b767f385b872e2a49415ea96b1d7346302
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-04-15 15:31:08 +0300

    Add CORS policy.

--- SimpleCAD/Program.cs
+++ SimpleCAD/Program.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.EntityFrameworkCore;
 using SimpleCAD.Data;
 
@@ -15,23 +14,32 @@ namespace SimpleCAD
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAngularApp", policy =>
+                {
+                    policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+            });
+
+            //builder.Services.AddEndpointsApiExplorer();
+            //builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                //app.UseSwagger();
+                //app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
-
+            app.UseCors("AllowAngularApp");
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
